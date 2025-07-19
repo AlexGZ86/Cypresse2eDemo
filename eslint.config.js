45bf59218ec41823bdb0a86556b124a8d081d8c2
alexgz86 <z.galindo.alex86@gmail.com>
2025-07-18 23:45:20 -0400

    Fix ESLint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,25 @@
-import globals from 'globals';
-import pluginReact from 'eslint-plugin-react';
-import pluginCypress from 'eslint-plugin-cypress';
-import pluginPrettier from 'eslint-plugin-prettier';
+import js from '@eslint/js';
 
 export default [
   {
     ignores: ['cypress/**', 'node_modules/**', '.idea/**'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        cy: 'readonly',
+        Cypress: 'readonly',
       },
     },
     plugins: {
-      react: pluginReact,
-      cypress: pluginCypress,
-      prettier: pluginPrettier,
+      react: require('eslint-plugin-react'),
+      cypress: require('eslint-plugin-cypress'),
+      prettier: require('eslint-plugin-prettier'),
     },
     rules: {
-      'no-console': 'warn',
-      'cypress/no-unnecessary-waiting': 'error',
       'prettier/prettier': 'error',
     },
   },
