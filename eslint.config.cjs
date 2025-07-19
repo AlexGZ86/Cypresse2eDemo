8a62d21484a69a774f0de7d6fd5e759c178eb400
alexgz86 <z.galindo.alex86@gmail.com>
2025-07-19 01:28:40 -0400

    Fix broken syntax in ESLint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,27 +1,27 @@
-const js = require('@eslint/js');
-const prettier = require('eslint-plugin-prettier');
-const cypress = require('eslint-plugin-cypress');
+const js = require("@eslint/js");
+const prettier = require("eslint-plugin-prettier");
+const cypress = require("eslint-plugin-cypress");
 
 module.exports = [
-    {
-        ignores: ['node_modules/**', '.idea/**'],
+  {
+    ignores: ["node_modules/**", ".idea/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        cy: "readonly",
+        Cypress: "readonly",
+      },
     },
-    {
-        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            globals: {
-                cy: 'readonly',
-                Cypress: 'readonly',
-            },
-        },
-        plugins: {
-            prettier,
-            cypress,
-        },
-        rules: {
-            'prettier/prettier': 'error',
-        },
+    plugins: {
+      prettier,
+      cypress,
     },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 ];
