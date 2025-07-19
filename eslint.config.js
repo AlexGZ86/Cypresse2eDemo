61ae24302468b569b88dbd5bf660beb1f95070a3
alexgz86 <z.galindo.alex86@gmail.com>
2025-07-18 23:52:09 -0400

    Fix ESLint config for ESM and plugin imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,28 @@
-import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import cypress from 'eslint-plugin-cypress';
+import prettier from 'eslint-plugin-prettier';
 
 export default [
-  {
-    ignores: ['cypress/**', 'node_modules/**', '.idea/**'],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        cy: 'readonly',
-        Cypress: 'readonly',
-      },
+    {
+        ignores: ['cypress/**', 'node_modules/**', '.idea/**'],
     },
-    plugins: {
-      react: require('eslint-plugin-react'),
-      cypress: require('eslint-plugin-cypress'),
-      prettier: require('eslint-plugin-prettier'),
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                cy: 'readonly',
+                Cypress: 'readonly',
+            },
+        },
+        plugins: {
+            react,
+            cypress,
+            prettier,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+        },
     },
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
 ];
