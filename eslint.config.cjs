33b46fbc38417d73b73844159356e09a38d746f4
alexgz86 <z.galindo.alex86@gmail.com>
2025-07-19 00:06:12 -0400

    Switch to CommonJS ESLint config to avoid ESM import issues

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,8 +1,9 @@
-import react from 'eslint-plugin-react';
-import cypress from 'eslint-plugin-cypress';
-import prettier from 'eslint-plugin-prettier';
+const js = require('@eslint/js');
+const react = require('eslint-plugin-react');
+const cypress = require('eslint-plugin-cypress');
+const prettier = require('eslint-plugin-prettier');
 
-export default [
+module.exports = [
     {
         ignores: ['cypress/**', 'node_modules/**', '.idea/**'],
     },
